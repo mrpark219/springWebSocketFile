eba5d397dfb5341cf03ce7783b4d3ceed30c8fab
mrpark219 <mrpark219@gmail.com>
2023-06-06 22:01:10 +0900

    🎉 스프링 웹 소켓 파일 전송 프로젝트 시작

---
+++ src/main/java/me/park/springwebsocketfile/SpringWebSocketFileApplication.java
@@ -0,0 +1,13 @@
+package me.park.springwebsocketfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocketFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/park/springwebsocketfile/SpringWebSocketFileApplicationTests.java
@@ -0,0 +1,13 @@
+package me.park.springwebsocketfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSocketFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
